90740c848b8e9eae45fbf0e40e252b3f06caa634
yjiaWu <yiw128@ucsd.edu>
2024-02-01 09:16:13 -0800

    bug for reverse method fixed

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -14,9 +14,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test 
+  public void testReversedFailed1(){
+    int[] input1 = {2,4,6};
+    assertArrayEquals(new int[] {6,4,2}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedFailed2(){
+    int[] input1 = {5,6,7,8,9,10};
+    assertArrayEquals(new int[] {10,9,8,7,6,5}, ArrayExamples.reversed(input1));
+  }
 }
+
+  
